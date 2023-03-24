fd4bd65e635c514ee986d75194f9aa8d26cb373b
Melanie <melanievandegraaf1@live.nl>
2023-03-24 12:31:19 +0100

    DAL and logic added

--- DAL/InventoryRepository.cs
+++ DAL/InventoryRepository.cs
@@ -19,8 +19,7 @@ public class InventoryRepository : IInventoryRepository
             using MySqlConnection connection = MySqlUtility.OpenConnection();
             using MySqlCommand? command = connection.CreateCommand();
             command.CommandText = "SELECT amount_of_sets, inventory_id, lego_set.* FROM inventory_lego_set INNER JOIN lego_set ON inventory_lego_set.lego_set_id = lego_set.id WHERE inventory_id=@inventoryId";
-            command.Parameters.AddWithValue("@inventoryId", id);
-
+            command.Parameters.AddWithValue("@inventoryId", id); 
             using MySqlDataReader? reader = command.ExecuteReader();
 
             if (!reader.HasRows)
@@ -55,11 +54,11 @@ public class InventoryRepository : IInventoryRepository
     {
         using MySqlConnection connection = MySqlUtility.OpenConnection();
         using MySqlCommand? command = connection.CreateCommand();
-        command.CommandText = "SELECT 1 FROM inventory_lego_set WHERE lego_set_id = @legoSetId";
+        command.CommandText = "SELECT EXISTS(SELECT * FROM inventory_lego_set WHERE lego_set_id = @legoSetId AND inventory_id = @inventoryId)";
         if ((int) command.ExecuteScalar() == 1)
         {
-            using MySqlCommand? updateCommand = connection.CreateCommand();
-            updateCommand.CommandText = "UPDATE";
+            using MySqlCommand? updateCommand = connection.CreateCommand();lego_set
+            updateCommand.CommandText = "UPDATE ";
         }
         else
         {
